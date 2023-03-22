c21374363b043f8e2b78653737c282a9102bd219
Dealer86 <adicroitor86@yahoo.com>
2023-03-22 18:33:48 +0200

    Added factory for users

--- finance-project/domain/user/factory.py
+++ finance-project/domain/user/factory.py
@@ -9,4 +9,12 @@ class UserFactory:
     def make(self, username: str) -> User:
         if len(username) < 6:
             raise InvalidUsername("Username should have at least 6 characters")
+        if len(username) > 20:
+            raise InvalidUsername("Username should have a maximum of 20 characters")
+        for char in username:
+            if not (char.isalnum() or char == "-"):
+                raise InvalidUsername(
+                    "Username should contain only alpha-numeric characters or '-'"
+                )
+
         return User(username)
