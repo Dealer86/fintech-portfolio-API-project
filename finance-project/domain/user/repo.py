388e7e2d04f6f0bf5f476b0840d1302447c8e41b
Dealer86 <adicroitor86@yahoo.com>
2023-03-22 18:34:56 +0200

    Added repo for users

---
+++ finance-project/domain/user/repo.py
@@ -0,0 +1,26 @@
+from domain.user.user import User
+import json
+
+
+class UserRepo:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        try:
+            file = open(file_path)
+            contents = file.read()
+            file.close()
+            users_info = json.loads(contents)
+            self.__users = [User(x) for x in users_info]
+        except:
+            self.__users = []
+
+    def add(self, new_user: User):
+        self.__users.append(new_user)
+        users_info = [x.username for x in self.__users]
+        users_json = json.dumps(users_info)
+        file = open(self.file_path, "w")
+        file.write(users_json)
+        file.close()
+
+    def get_all(self) -> list[User]:
+        return self.__users
