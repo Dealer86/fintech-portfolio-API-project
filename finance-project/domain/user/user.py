d635bb4fd2385c849a684b8570f76109158a58e6
Dealer86 <adicroitor86@yahoo.com>
2023-03-27 17:36:36 +0300

    Added id populated with uuid

--- finance-project/domain/user/user.py
+++ finance-project/domain/user/user.py
@@ -1,7 +1,11 @@
+import uuid
+
+
 class User:
-    def __init__(self, username: str, stocks: list[str] = None):
+    def __init__(self, username: str, stocks: list[str] = None, id_: str = None):
         self.__username = username
         self.__stocks = stocks if stocks else []
+        self.__id_ = id_ if id_ is not None else str(uuid.uuid4())
 
     @property
     def username(self) -> str:
@@ -10,3 +14,7 @@ class User:
     @property
     def stocks(self) -> list[str]:
         return self.__stocks
+
+    @property
+    def id(self):
+        return self.__id_
