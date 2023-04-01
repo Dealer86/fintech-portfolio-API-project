40f1c4b1071a7a71b40ec528d086c047447dc69d
Dealer86 <adicroitor86@yahoo.com>
2023-04-01 12:43:06 +0300

    Clean name

--- finance-project/domain/asset/factory.py
+++ finance-project/domain/asset/factory.py
@@ -1,5 +1,6 @@
 import yahooquery
 from domain.asset.asset import Asset
+import pandas as pd
 
 
 class AssetFactory:
@@ -8,6 +9,8 @@ class AssetFactory:
         # TODO error handling & tests
         t = yahooquery.Ticker(ticker)
         profile = t.summary_profile[ticker]
+
+        print(profile)
         name = self.__extract_name(profile)
         country = profile["country"]
         sector = profile["sector"]
@@ -25,4 +28,5 @@ class AssetFactory:
         words = summary.split(" ")
         first_2_words = words[0:2]
         name = " ".join(first_2_words)
+        name = name.replace(",", "")
         return name
