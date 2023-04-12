65290994f662daa248bf75fb5436fe03321de6d0
Dealer86 <adicroitor86@yahoo.com>
2023-04-12 14:06:18 +0300

    Removed one line

--- finance-project/domain/asset/factory.py
+++ finance-project/domain/asset/factory.py
@@ -8,8 +8,6 @@ class AssetFactory:
         # TODO error handling & tests
         t = yahooquery.Ticker(ticker)
         profile = t.summary_profile[ticker]
-
-        print(profile)
         name = self.__extract_name(profile)
         country = profile["country"]
         sector = profile["sector"]
