ec2033f825022b17dfba05e5f46accf5616af794
Dealer86 <adicroitor86@yahoo.com>
2023-04-12 14:07:43 +0300

    Add try except & rename database

--- finance-project/domain/asset/repo.py
+++ finance-project/domain/asset/repo.py
@@ -6,7 +6,7 @@ from domain.user.user import User
 class AssetRepo:
     def add_to_user(self, user: User, asset: Asset):
         table = f"{user.id}-assets".replace("-", "_")
-        with sqlite3.connect(f"main_assets.db") as conn:
+        with sqlite3.connect(f"main_users.db") as conn:
             cursor = conn.cursor()
             try:
                 cursor.execute(
@@ -14,7 +14,8 @@ class AssetRepo:
                     f"VALUES ('{asset.ticker}', '{asset.name}', "
                     f"'{asset.country}', {asset.units})"
                 )
-            except sqlite3.OperationalError:
+            except Exception as e:
+                print("Failed to add asset" +str(e))
                 cursor.execute(
                     f"CREATE TABLE '{table}'"
                     f" (ticker TEXT PRIMARY KEY,"
@@ -31,9 +32,15 @@ class AssetRepo:
 
     def get_for_user(self, user: User) -> list[Asset]:
         table = f"{user.id}-assets".replace("-", "_")
-        with sqlite3.connect(f"main_assets.db") as conn:
+        with sqlite3.connect(f"main_users.db") as conn:
             cursor = conn.cursor()
-            cursor.execute(f"SELECT * FROM '{table}'")
+            try:
+                cursor.execute(f"SELECT * FROM '{table}'")
+            except sqlite3.OperationalError as e:
+                if "no such table" in str(e):
+                    return []
+                else:
+                    raise e
             assets_info = cursor.fetchall()
         assets = [
             Asset(ticker=x[0], nr=x[3], name=x[1], country=x[2], sector="sec")
