5d508cac7725f09529992594b4495825d4acba8a
Dealer86 <adicroitor86@yahoo.com>
2023-04-24 15:20:45 +0300

    Add delete get update for cache

--- finance-project/domain/user/repo.py
+++ finance-project/domain/user/repo.py
@@ -25,11 +25,26 @@ class UserRepo:
 
     def get_by_id(self, uid: str) -> User:
         self.__check_we_have_users()
-        return self.__persistence.get_by_id(uid)
+        for user in self.__users:
+            if str(user.id) == uid:
+                return user
+            else:
+                return self.__persistence.get_by_id(uid)
 
-    def delete_by_id(self, uid: str):
+    def delete(self, uid: str):
         self.__check_we_have_users()
-        self.__persistence.delete_by_id(uid)
+        self.__persistence.delete(uid)
+        for user in self.__users:
+            if user.id == uuid.UUID(hex=uid):
+                self.__users.remove(user)
+                break
+
+    def update(self, user_id: str, username: str):
+        self.__check_we_have_users()
+        self.__persistence.update(user_id, username)
+        for user in self.__users:
+            if user.id == uuid.UUID(hex=user_id):
+                user.username = username
 
     def __check_we_have_users(self):
         if self.__users is None:
