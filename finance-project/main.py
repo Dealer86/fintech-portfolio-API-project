9ef07c4398556d9c767933dda5aa6a7a8f85cf79
Dealer86 <adicroitor86@yahoo.com>
2023-04-24 15:32:54 +0300

    Add new version 0.4.0

--- finance-project/main.py
+++ finance-project/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
     title="Fintech Portfolio API",
     description="A webserver with a REST API for keeping track of your different financial assets,"
     " stocks & crypto, and see/compare their evolution",
-    version="0.3.9",
+    version="0.4.0",
 )
 
 app.include_router(users_router)
