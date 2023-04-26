b35acd05fa9d070758d4026a8ebf0f8be8b90711
Dealer86 <adicroitor86@yahoo.com>
2023-04-26 17:39:58 +0300

    Add more exceptions

--- finance-project/domain/exceptions.py
+++ finance-project/domain/exceptions.py
@@ -4,3 +4,15 @@ class InvalidUsername(Exception):
 
 class InvalidTicker(Exception):
     pass
+
+
+class NonExistentUserId(Exception):
+    pass
+
+
+class DuplicateUser(Exception):
+    pass
+
+
+class DuplicateAsset(Exception):
+    pass
