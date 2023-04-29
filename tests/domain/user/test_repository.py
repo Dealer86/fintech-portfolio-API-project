f18b85851a040c7a6b38f4cd31a9ca42adb176db
Dealer86 <adicroitor86@yahoo.com>
2023-04-29 13:28:14 +0300

    "In progress"

--- tests/domain/user/test_repository.py
+++ tests/domain/user/test_repository.py
@@ -1,32 +1,21 @@
 import unittest
 
-from domain.user.factory import UserFactory
-from domain.user.repo import UserRepo
+
+
+# TODO make test for UserRepo
 
 
 class UserRepoTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.users_file = "test_users.json"
-        cls.repo = UserRepo(cls.users_file)
+        pass
 
     def test_it_adds_a_user(self):
-        expected_username = "a-username"
-        new_user = UserFactory().make_new(expected_username)
-
-        self.repo.add(new_user)
-
-        actual_users = self.repo.get_all()
-        self.assertEqual(1, len(actual_users))
-        self.assertEqual(expected_username, actual_users[0].username)
+        pass
 
     def test_it_reads_a_user_from_the_system(self):
-        repo = UserRepo(self.users_file)
+        pass
 
-        actual_users = repo.get_all()
-        self.assertEqual(1, len(actual_users))
-
-    # TODO homework
     @unittest.skip
     def test_it_deletes_a_user_from_system(self):
         pass
