2ff1fdafb43bbd94cd154e463050788a6e1ac7de
Dealer86 <adicroitor86@yahoo.com>
2023-05-01 15:32:56 +0300

    Add persistence interface

---
+++ finance-project/domain/asset/persistence_interface.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+from domain.asset.asset import Asset
+from domain.user.user import User
+
+
+class AssetPersistenceInterface(ABC):
+    @abstractmethod
+    def add_to_user(self, user: User, asset: Asset):
+        pass
+
+    @abstractmethod
+    def get_for_user(self, user: User) -> list[Asset]:
+        pass
