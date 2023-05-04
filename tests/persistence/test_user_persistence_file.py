91ebcc1a78481dbee3576af332f2410d31df212f
Dealer86 <adicroitor86@yahoo.com>
2023-05-04 21:27:29 +0300

    "Add test for user persistence file"

--- tests/persistence/test_user_persistence_file.py
+++ tests/persistence/test_user_persistence_file.py
@@ -10,64 +10,50 @@ class UserPersistenceFileTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.users_file = "test_users.json"
-        cls.repo = UserPersistenceFile(cls.users_file)
+        cls.persistence_file = UserPersistenceFile(cls.users_file)
 
     def test_it_adds_a_user(self):
         expected_username = "a-username"
         new_user = UserFactory().make_new(expected_username)
 
-        self.repo.add(new_user)
+        self.persistence_file.add(new_user)
 
-        actual_users = self.repo.get_all()
+        actual_users = self.persistence_file.get_all()
         self.assertEqual(1, len(actual_users))
         self.assertEqual(expected_username, actual_users[0].username)
 
-    def test_it_reads_users_from_the_system(self):
+    def test_it_reads_users_from_file(self):
         a_username = "a-username"
         new_user = UserFactory().make_new(a_username)
 
-        self.repo.add(new_user)
+        self.persistence_file.add(new_user)
 
-        actual_users = self.repo.get_all()
+        actual_users = self.persistence_file.get_all()
 
         self.assertIsNotNone(actual_users)
         self.assertIn(new_user.username, actual_users[0].username)
 
-    def test_it_deletes_a_user_from_system(self):
+    def test_it_deletes_a_user_from_file(self):
         a_username = "a-username"
         new_user = UserFactory().make_new(a_username)
 
-        self.repo.add(new_user)
+        self.persistence_file.add(new_user)
 
-        self.repo.delete(str(new_user.id))
+        self.persistence_file.delete(str(new_user.id))
 
-        all_id_from_system = [str(u.id) for u in self.repo.get_all()]
+        all_id_from_system = [str(u.id) for u in self.persistence_file.get_all()]
         self.assertNotIn(str(new_user.id), all_id_from_system)
 
-    def test_it_gets_a_user_by_id_from_system(self):
-        a_username = "a-username"
-        new_user = UserFactory().make_new(a_username)
-        expected_id = new_user.id
+    def test_it_updates_a_username_to_file(self):
+        new_user = UserFactory().make_new("username-before")
 
-        self.repo.add(new_user)
+        self.persistence_file.add(new_user)
 
-        actual_user = self.repo.get_by_id(str(new_user.id))
-        actual_id = actual_user.id
+        self.persistence_file.update(str(new_user.id), "updated-username")
 
-        self.assertEqual(expected_id, actual_id)
+        all_usernames = [x.username for x in self.persistence_file.get_all()]
 
-    def test_it_updates_a_users_username_to_system(self):
-        a_username = "username-before"
-        new_user = UserFactory().make_new(a_username)
-        username_before_update = new_user.username
-
-        self.repo.add(new_user)
-
-        self.repo.update(str(new_user.id), "updated-username")
-        actual_user = self.repo.get_by_id(str(new_user.id))
-        username_after_update = actual_user.username
-
-        self.assertNotEqual(username_before_update, username_after_update)
+        self.assertIn("updated-username", all_usernames)
 
     @classmethod
     def tearDownClass(cls) -> None:
