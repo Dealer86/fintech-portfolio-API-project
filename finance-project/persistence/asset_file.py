eeafe58999c8611e8318b268449cf628bab336f5
Dealer86 <adicroitor86@yahoo.com>
2023-05-07 14:31:34 +0300

    "Add AssetPersistenceFile"

--- finance-project/persistence/asset_file.py
+++ finance-project/persistence/asset_file.py
@@ -1,17 +1,59 @@
 import json
 
 from domain.asset.asset import Asset
-from domain.asset.factory import AssetFactory
 from domain.asset.persistence_interface import AssetPersistenceInterface
 from domain.user.user import User
 
 
+class DuplicateAsset(Exception):
+    pass
+
+
 class AssetPersistenceFile(AssetPersistenceInterface):
-    def __init__(self, file_path: str):
-        self.__file_path = file_path
+    def __init__(self, filename: str):
+        self.__filename = filename
 
     def add_to_user(self, user: User, asset: Asset):
-        pass
+        self.__write_to_file(user, asset)
 
     def get_for_user(self, user: User) -> list[Asset]:
-        pass
+        data = self.__load_json()
+        user_assets = data.get(str(user.id), [])
+        assets = []
+        for asset_dict in user_assets:
+            asset = Asset(
+                ticker=asset_dict["ticker"],
+                name=asset_dict["name"],
+                country=asset_dict["country"],
+                nr=asset_dict["nr"],
+                sector=asset_dict["sector"],
+            )
+            assets.append(asset)
+        return assets
+
+    def __load_json(self) -> dict[str, list[dict[str, any]]]:
+        try:
+            with open(self.__filename) as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return {}
+
+    def __write_to_file(self, user: User, asset: Asset):
+        data = self.__load_json()
+        user_assets = data.get(str(user.id), [])
+        for every_dict in user_assets:
+            print(every_dict)
+            if every_dict["ticker"] == asset.ticker:
+                raise DuplicateAsset(f"Asset {asset.ticker} already added")
+        user_assets.append(
+            {
+                "ticker": asset.ticker,
+                "name": asset.name,
+                "country": asset.country,
+                "nr": asset.units,
+                "sector": asset.sector,
+            }
+        )
+        data[str(user.id)] = user_assets
+        with open(self.__filename, "w") as f:
+            json.dump(data, f, indent=4)
