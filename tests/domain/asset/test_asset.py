4eb0f2fa29b687ee0fdbdbc8299f1787ccd21a84
Dealer86 <adicroitor86@yahoo.com>
2023-05-08 12:29:45 +0300

    "Add percentage difference test"

--- tests/domain/asset/test_asset.py
+++ tests/domain/asset/test_asset.py
@@ -9,9 +9,7 @@ class AssetTestCase(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.__asset = Asset("AAPL", 10, "Apple Inc.", "US", "Technology")
 
-    def test_ticker(
-        self,
-    ):
+    def test_ticker(self):
         self.assertEqual(self.__asset.ticker, "AAPL")
 
     def test_nr(self):
@@ -74,6 +72,29 @@ class AssetTestCase(unittest.TestCase):
         mock_yfinance.return_value.fast_info = {"fiftyDayAverage": expected}
         asset = Asset("goog", 1, "Google", "US", "Tech")
         self.assertEqual(asset.fifty_day_price, expected)
+
+    @patch("yfinance.Ticker")
+    def test_percentage_difference_when_current_price_is_higher(self, mock_yfinance):
+        mock_yfinance.return_value.fast_info = {"lastPrice": 102.12, "previousClose": 99.10}
+        asset = Asset("goog", 1, "Google", "US", "Tech")
+        expected = f'The closed price {asset.closed_price} is {abs(((asset.closed_price - asset.current_price) / asset.closed_price) * 100):.2f}% lower than current price {asset.current_price}'
+
+        self.assertEqual(asset.percentage_difference_between_closed_and_current_price, expected)
+
+    @patch("yfinance.Ticker")
+    def test_percentage_difference_when_current_price_is_lower(self, mock_yfinance):
+        mock_yfinance.return_value.fast_info = {"lastPrice": 99.12, "previousClose": 103.10}
+        asset = Asset("goog", 1, "Google", "US", "Tech")
+        expected = f'The closed price {asset.closed_price} is {(((asset.closed_price - asset.current_price) / asset.closed_price) * 100):.2f}% higher than current price {asset.current_price}'
+
+        self.assertEqual(asset.percentage_difference_between_closed_and_current_price, expected)
+
+    @patch("yfinance.Ticker")
+    def test_percentage_difference_when_current_equal_closed_price(self, mock_yfinance):
+        mock_yfinance.return_value.fast_info = {"lastPrice": 100.00, "previousClose": 100.00}
+        asset = Asset("tsla", 2, "Tesla, Inc", "US", "Technology")
+        expected = "Values are the same"
+        self.assertEqual(asset.percentage_difference_between_closed_and_current_price, expected)
 
 
 if __name__ == "__main__":
