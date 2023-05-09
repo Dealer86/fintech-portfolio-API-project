cd3fde0d75144017b7c94a463eb3d7cb69bf669b
Dealer86 <adicroitor86@yahoo.com>
2023-05-09 12:49:43 +0300

    "Add delete_for_user method"

--- finance-project/domain/asset/repo.py
+++ finance-project/domain/asset/repo.py
@@ -14,11 +14,14 @@ class AssetRepo:
     def add_to_user(self, user: User, asset: Asset):
         self.__check_we_have_assets(user)
         self.__persistence.add_to_user(user, asset)
-        self.__assets.append([user, asset])
+        self.__assets.append(asset)
 
     def get_for_user(self, user: User) -> list[Asset]:
         self.__check_we_have_assets(user)
         return self.__assets
+
+    def delete_for_user(self, user_id: str, asset: str):
+        self.__persistence.delete_for_user(user_id, asset)
 
     def __check_we_have_assets(self, user: User):
         if self.__assets is None:
