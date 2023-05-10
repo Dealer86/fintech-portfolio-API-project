2e819e03bcc62a8e699a4f3e8d640e610c8ae9cb
Dealer86 <adicroitor86@yahoo.com>
2023-05-10 10:54:17 +0300

    "Add repo changes"

--- finance-project/domain/asset/repo.py
+++ finance-project/domain/asset/repo.py
@@ -14,7 +14,7 @@ class AssetRepo:
     def add_to_user(self, user: User, asset: Asset):
         self.__check_we_have_assets(user)
         self.__persistence.add_to_user(user, asset)
-        self.__assets.append(asset)
+        self.__assets.append([user, asset])
 
     def get_for_user(self, user: User) -> list[Asset]:
         self.__check_we_have_assets(user)
@@ -23,6 +23,9 @@ class AssetRepo:
     def delete_for_user(self, user_id: str, asset: str):
         self.__persistence.delete_for_user(user_id, asset)
 
+
     def __check_we_have_assets(self, user: User):
         if self.__assets is None:
             self.__assets = self.__persistence.get_for_user(user)
+
+
