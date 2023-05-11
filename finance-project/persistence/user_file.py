a97626a241d11a0c609531556390a1402c9f57f6
Dealer86 <adicroitor86@yahoo.com>
2023-05-11 18:06:19 +0300

    "Add logging"

--- finance-project/persistence/user_file.py
+++ finance-project/persistence/user_file.py
@@ -20,11 +20,13 @@ class UserPersistenceFile(UserPersistenceInterface):
     def get_all(self) -> list[User]:
         if not os.path.exists(self.__file_path):
             return []
+        logging.info("UserPersistenceFile executing get all command...")
         try:
             with open(self.__file_path) as f:
                 contents = f.read()
             users_info = json.loads(contents)
             factory = UserFactory()
+            logging.info("UserPersistenceFile get all command was successfully executed")
             return [factory.make_from_persistence(x) for x in users_info]
 
         except FailToWriteToFile as e:
@@ -35,6 +37,7 @@ class UserPersistenceFile(UserPersistenceInterface):
             return []
 
     def add(self, user: User):
+        logging.info("UserPersistenceFile executing add command...")
         current_users = self.get_all()
         current_users.append(user)
         users_info = [(str(x.id), x.username, x.stocks) for x in current_users]
@@ -42,10 +45,12 @@ class UserPersistenceFile(UserPersistenceInterface):
         try:
             with open(self.__file_path, "w") as f:
                 f.write(users_json)
+                logging.info(f"UserPersistenceFile add command successfully executed, added {user.username} to file")
         except FailToWriteToFile as e:
             logging.error("Could not write file. Error: " + str(e))
 
     def delete(self, uid: str):
+        logging.info("UserPersistenceFile executing delete command...")
         current_users = self.get_all()
         updated_users_list = [u for u in current_users if u.id != uuid.UUID(hex=uid)]
         users_info = [(str(x.id), x.username, x.stocks) for x in updated_users_list]
@@ -53,10 +58,12 @@ class UserPersistenceFile(UserPersistenceInterface):
         try:
             with open(self.__file_path, "w") as f:
                 f.write(json_current_users)
+                logging.info("UserPersistenceFile delete command successfully executed")
         except FailToWriteToFile as e:
             logging.error("Could not delete from file. Error: " + str(e))
 
     def update(self, user_id: str, new_username: str):
+        logging.info("UserPersistenceFile executing update command...")
         current_users = self.get_all()
         for user in current_users:
             if user.id == uuid.UUID(hex=user_id):
@@ -67,5 +74,6 @@ class UserPersistenceFile(UserPersistenceInterface):
         try:
             with open(self.__file_path, "w") as f:
                 f.write(users_json)
+                logging.info("UserPersistenceFile update command successfully executed")
         except FailToWriteToFile as e:
             logging.error("Could not update file. Error: " + str(e))
