ce50f857d31f6445231774b1cf40dce25411ffc0
Dealer86 <adicroitor86@yahoo.com>
2023-05-11 18:36:44 +0300

    "Add pretty indent 4"

--- finance-project/persistence/user_file.py
+++ finance-project/persistence/user_file.py
@@ -41,26 +41,28 @@ class UserPersistenceFile(UserPersistenceInterface):
         current_users = self.get_all()
         current_users.append(user)
         users_info = [(str(x.id), x.username, x.stocks) for x in current_users]
-        users_json = json.dumps(users_info)
+        users_json = json.dumps(users_info, indent=4)
         try:
             with open(self.__file_path, "w") as f:
                 f.write(users_json)
                 logging.info(f"UserPersistenceFile add command successfully executed, added {user.username} to file")
         except FailToWriteToFile as e:
             logging.error("Could not write file. Error: " + str(e))
+            raise e
 
     def delete(self, uid: str):
         logging.info("UserPersistenceFile executing delete command...")
         current_users = self.get_all()
         updated_users_list = [u for u in current_users if u.id != uuid.UUID(hex=uid)]
         users_info = [(str(x.id), x.username, x.stocks) for x in updated_users_list]
-        json_current_users = json.dumps(users_info)
+        json_current_users = json.dumps(users_info, indent=4)
         try:
             with open(self.__file_path, "w") as f:
                 f.write(json_current_users)
                 logging.info("UserPersistenceFile delete command successfully executed")
         except FailToWriteToFile as e:
             logging.error("Could not delete from file. Error: " + str(e))
+            raise e
 
     def update(self, user_id: str, new_username: str):
         logging.info("UserPersistenceFile executing update command...")
@@ -70,10 +72,11 @@ class UserPersistenceFile(UserPersistenceInterface):
                 user.username = new_username
                 break
         users_info = [(str(u.id), u.username, u.stocks) for u in current_users]
-        users_json = json.dumps(users_info)
+        users_json = json.dumps(users_info, indent=4)
         try:
             with open(self.__file_path, "w") as f:
                 f.write(users_json)
                 logging.info("UserPersistenceFile update command successfully executed")
         except FailToWriteToFile as e:
             logging.error("Could not update file. Error: " + str(e))
+            raise e
