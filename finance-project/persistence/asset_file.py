619d40e7d0965b9fa40b3a67a74f7534db8ced89
Dealer86 <adicroitor86@yahoo.com>
2023-05-29 18:12:18 +0300

    Add delete get add to user methods that use get_all

--- finance-project/persistence/asset_file.py
+++ finance-project/persistence/asset_file.py
@@ -1,35 +1,49 @@
 import json
 import logging
+import os
 
-from domain.asset.factory import AssetFactory
+
 from domain.user.factory import UserFactory
 from domain.asset.asset import Asset
 from domain.asset.persistence_interface import AssetPersistenceInterface
-from domain.exceptions import DuplicateAsset
+
 from domain.user.user import User
-from persistence.user_file import UserPersistenceFile
 
 
 class AssetPersistenceFile(AssetPersistenceInterface):
     def __init__(self, filename: str):
         self.__filename = filename
 
+    def get_all(self) -> list[User]:
+        if not os.path.exists(self.__filename):
+            return []
+        with open(self.__filename) as f:
+            content = f.read()
+        data = json.loads(content)
+        return [UserFactory.from_tuple(u) for u in data]
+
     def add_to_user(self, user: User, asset: Asset):
-        user_repo = UserPersistenceFile("main_users.json")
-        user_list = user_repo.get_all()
+        user_list = self.get_all()
         for u in user_list:
             if str(u.id) == str(user.id):
                 u.stocks.append(asset)
+        self.__save_to_file(user_list)
 
-        self.save_to_file(user_list)
-
-    def delete_for_user(self, user_id: str, asset: Asset):
-        pass
+    def delete_for_user(self, user_id: str, asset: str):
+        user_list = self.get_all()
+        for user in user_list:
+            if str(user.id) == user_id:
+                user.stocks = [a for a in user.stocks if a.ticker != asset]
+                break
+        self.__save_to_file(user_list)
 
     def get_for_user(self, user: User) -> list[Asset]:
-        return user.stocks
+        user_data = self.get_all()
+        for u in user_data:
+            if str(u.id) == str(user.id):
+                return u.stocks
 
-    def save_to_file(self, user_list: list[User]):
+    def __save_to_file(self, user_list: list[User]):
         data = [u.to_tuple() for u in user_list]
         data_prep = json.dumps(data, indent=4)
 
