81fb550d98b7ebc31838a2b0b3f3cb3e416b1bc8
Dealer86 <adicroitor86@yahoo.com>
2023-07-21 10:46:52 +0300

    Refactored singleton

--- finance-project/singleton.py
+++ finance-project/singleton.py
@@ -1,17 +1,9 @@
 def singleton(cls):
     instances = {}
-    print("After declaration")
-    print(instances)
-    print(cls)
 
     def wrapper(*args, **kwargs):
         if cls not in instances:
-            print(*args, **kwargs)
             instances[cls] = cls(*args, **kwargs)
-        print("Before return")
-        print(instances)
         return instances[cls]
 
     return wrapper
-
-
