3f757539c73c4ff1c8b27a3c49a70a6e95c90504
Dealer86 <adicroitor86@yahoo.com>
2023-08-03 11:25:08 +0300

    Added grid, fig size, title, x and y label

--- finance-project/api/assets.py
+++ finance-project/api/assets.py
@@ -18,10 +18,16 @@ def get_asset(ticker: str):
 @assets_router.get("/{ticker}/history")
 def get_history(ticker: str, start_date: str = "2019-01-30", end_date: str = "2023-04-10"):
     t = yfinance.Ticker(ticker)
-    print(start_date)
     history = t.history(interval="1d", start=start_date, end=end_date)
     data = (history["Open"])
+    pyplot.figure(figsize=(12, 6))
+
     pyplot.plot(data)
+    pyplot.title(f"{ticker.capitalize()} Price History")
+    pyplot.xlabel("Date")
+    pyplot.ylabel("Price")
+    pyplot.grid(True)
+    pyplot.tight_layout()
     image_name = f"{ticker} - {uuid.uuid4()}.png"
     pyplot.savefig(image_name)
     pyplot.clf()
