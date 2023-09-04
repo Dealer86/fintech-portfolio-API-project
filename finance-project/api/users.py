948c2b7e37608d5c6f505cf4d405a9d3eab55c6e
Dealer86 <adicroitor86@yahoo.com>
2023-09-04 12:44:43 +0300

    Added logging and exception handling for difference scenarios

--- finance-project/api/users.py
+++ finance-project/api/users.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from configuration.asset_config import set_asset_persistence_type
 from configuration.config import set_persistence_type
@@ -39,25 +39,58 @@ def get_user_repo() -> UserRepo:
 
 @users_router.get("", response_model=list[UserInfo])
 def get_all_users(repo=Depends(get_user_repo)):
-    return repo.get_all()
+    logging.info("Fetching all users...")
+    try:
+        users = repo.get_all()
+        logging.info("Successfully fetched all users")
+        return users
+    except Exception as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @users_router.get("/{user_id}", response_model=UserInfo)
 def get_user(user_id: str, repo=Depends(get_user_repo)):
-    return repo.get_by_id(user_id)
+    logging.info(f"Fetching user with ID {user_id}...")
+    try:
+        user = repo.get_by_id(user_id)
+        logging.info(f"Successfully fetched user with ID {user_id}")
+        return user
+    except NonExistentUserId as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @users_router.delete("/{user_id}")
 def delete_user(user_id: str, repo=Depends(get_user_repo)):
     logging.info(f"Deleting user with id {user_id}")
-    repo.delete(user_id)
-    return {"status": "ok"}
+    try:
+        repo.delete(user_id)
+        logging.info(f"Successfully deleted user with id {user_id}")
+        return {"status": f"Successfully deleted user with id {user_id}"}
+    except NonExistentUserId as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @users_router.delete("/{user_id}/assets")
 def delete_asset_for_user(user_id: str, asset: str, asset_repo=Depends(get_asset_repo)):
     logging.info(f"Deleting asset {asset} for user with id {user_id}")
-    asset_repo.delete_for_user(user_id, asset)
+    try:
+        asset_repo.delete_for_user(user_id, asset)
+        logging.info(f"Succesfully deleted asset {asset} for user with id {user_id}")
+    except KeyError as ke:
+        logging.error(str(e))
+        raise HTTPException(status_code=404, detail=str(ke))
+    except Exception as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @users_router.patch("/{user_id}/username", response_model=UserInfo)
@@ -66,10 +99,12 @@ def update_user(user_id: str, username: str, repo=Depends(get_user_repo)):
     try:
         repo.update(user_id, username)
         logging.info(f"Successfully update user {username} with ID {user_id}")
+        return repo.get_by_id(user_id)
     except NonExistentUserId as e:
         logging.error(str(e))
-        raise e
-    return repo.get_by_id(user_id)
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @users_router.patch("/{users_id}/units_number", response_model=UserInfo)
@@ -80,25 +115,38 @@ def update_unit_number_of_assets_for_user(
     asset_repo=Depends(get_asset_repo),
     repo=Depends(get_user_repo),
 ):
-    our_user = repo.get_by_id(users_id)
-    asset_repo.update_unit_number_of_assets_for_user(our_user, asset, units_number)
-
-    return repo.get_by_id(users_id)
+    logging.info(
+        f"Updating units number for asset '{asset}' of user with ID {users_id}..."
+    )
+    try:
+        our_user = repo.get_by_id(users_id)
+        asset_repo.update_unit_number_of_assets_for_user(our_user, asset, units_number)
+        logging.info(
+            f"Updated units number for asset '{asset}' of user with ID {users_id} successfully."
+        )
+        return repo.get_by_id(users_id)
+    except NonExistentUserId as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @users_router.post("", response_model=UserInfo)
 def create_a_user(new_user: UserAdd, repo=Depends(get_user_repo)):
-    logging.info("Creating a user...")
-    user = UserFactory().make_new(new_user.username)
+    logging.info(f"Creating user with username: '{new_user.username}'...")
     try:
+        user = UserFactory().make_new(new_user.username)
         repo.add(user)
         logging.info(f"Successfully created user {user.username}")
+        return user
     except DuplicateUser as e:
         logging.error(str(e))
-        raise e
+        raise HTTPException(status_code=409, detail=str(e))
     except Exception as e:
         logging.error("Error could not create a user. Reason: " + str(e))
-    return user
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @users_router.post("/{user_id}/assets", response_model=AssetInfoUser)
@@ -112,21 +160,22 @@ def add_asset_to_user(
     try:
         new_asset = AssetFactory().make_new(asset.ticker)
         logging.info(f"Successfully created asset {asset.ticker}")
-    except TypeError:
+    except InvalidTicker as e:
         logging.warning(f"Invalid ticker {asset.ticker}")
-        raise InvalidTicker(f"Invalid ticker {asset.ticker}")
+        raise HTTPException(status_code=400, detail=str(e))
     logging.info(f"Getting user with id {user_id}...")
     try:
         user = repo.get_by_id(user_id)
         logging.info(f"User {user.username} with id {user_id} found")
     except NonExistentUserId as e:
         logging.error(str(e))
-        raise e
+        raise HTTPException(status_code=404, detail=str(e))
+
     logging.info(f"Adding asset {asset.ticker} to user {user.username}...")
     try:
         asset_repo.add_to_user(user, new_asset)
         logging.info(f"Successfully added asset {asset.ticker} to user {user.username}")
     except DuplicateAsset as e:
         logging.error(str(e))
-        raise e
+        raise HTTPException(status_code=409, detail=str(e))
     return new_asset
