dec1d8113512390eb57bbb8727cb875a831bf00c
Dealer86 <adicroitor86@yahoo.com>
2023-09-05 19:11:29 +0300

    Added logging for UserRepo for all methods

--- finance-project/domain/user/repo.py
+++ finance-project/domain/user/repo.py
@@ -17,13 +17,14 @@ class UserRepo:
     def __init__(
         self, persistence: UserPersistenceInterface, asset: AssetPersistenceInterface
     ):
-        print("Initializing user repo")
         self.__persistence = persistence
         self.__users = None
         self.__asset = asset
 
     def add(self, new_user: User):
-        logging.info("UserRepo executing add command...")
+        logging.info(
+            f"UserRepo executing add command for user with id {str(new_user.id)}..."
+        )
         self.__check_we_have_users()
         if new_user.username in [u.username for u in self.__users]:
             logging.error("UserRepo add command failed because of duplicate user")
@@ -39,17 +40,20 @@ class UserRepo:
     def get_all(self) -> list[User]:
         logging.info("UserRepo executing get all command...")
         self.__check_we_have_users()
+        logging.info("UserRepo successfully executed get all command")
         return self.__users
 
     def get_by_id(self, uid: str) -> User:
-        logging.info("UserRepo executing get by id command...")
+        logging.info(f"UserRepo executing get by id command for user with id {uid}...")
         self.__check_we_have_users()
         self.__check_id(uid)
 
         for u in self.__users:
             if u.id == uuid.UUID(hex=uid):
                 assets = self.__asset.get_for_user(u)
-                logging.info("UserRepo get by id command was successfully executed")
+                logging.info(
+                    f"UserRepo get by id command was successfully executed for user with id {uid}"
+                )
                 return User(
                     uuid=u.id,
                     username=u.username,
@@ -57,20 +61,24 @@ class UserRepo:
                 )
 
     def delete(self, uid: str):
-        logging.info("UserRepo executing delete command...")
+        logging.info(f"UserRepo executing delete command for user with id {uid}...")
         self.__check_we_have_users()
         self.__check_id(uid)
         self.__persistence.delete(uid)
         self.__refresh_cache()
-        logging.info("UserRepo delete command was successfully executed")
+        logging.info(
+            f"UserRepo delete command was successfully executed for user with id {uid}"
+        )
 
     def update(self, user_id: str, username: str):
-        logging.info("UserRepo executing update command...")
+        logging.info(f"UserRepo executing update command for user with id {user_id}...")
         self.__check_we_have_users()
         self.__check_id(user_id)
         self.__persistence.update(user_id, username)
+        logging.info(
+            f"UserRepo update command was successfully executed for user with id {user_id}"
+        )
         self.__refresh_cache()
-        logging.info("UserRepo update command was successfully executed")
 
     def __check_we_have_users(self):
         if self.__users is None:
